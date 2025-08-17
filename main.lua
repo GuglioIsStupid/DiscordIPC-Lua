1af9bb7f5e5f851187ad943fca089e3ca2f5a948
GuglioIsStupid <bradsbluetab@gmail.com>
2025-08-17 18:59:56 -0400

    Update example

--- main.lua
+++ main.lua
@@ -4,13 +4,36 @@ discordIPC:initID("746227477801861142")
 discordIPC:connect()
 
 discordIPC.activity = {
+    state = "I feel the music",
     details = "WOW!",
     timestamps = {
-        start = os.time() * 1000
+        start = os.time() * 1000,
+        ["end"] = os.time() * 1000 + 3600000
     },
     assets = {
-        large_image = "dj-pp-_-i-feel-the-music"
-    }
+        large_image = "test",
+        small_image = "test",
+        large_text = "test",
+        small_text = "test"
+    },
+    --[[ 
+    party = {
+        id = "test",
+        size = {
+            1,
+            2
+        }
+    }, ]]
+    --[[
+    secrets = {
+    join = "test",
+    spectate = "test",
+    match = "test"
+    },
+    ]]
+    --[[
+    instance = false
+    ]]
 }
 
 discordIPC:sendActivity()
